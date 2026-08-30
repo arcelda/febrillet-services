552637d0e045293a60a2372c36c512e9451a61e7
Alex J Reyes <arceldajactta@gmail.com>
2026-08-29 20:14:25 -0400

    Setting up the site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Hero } from "@/components/Hero";
+import { TrustBadges } from "@/components/TrustBadges";
 import { ServicesGrid } from "@/components/ServicesGrid";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <TrustBadges />
       <ServicesGrid />
     </main>
   );
